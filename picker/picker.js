5e65ecaf6d79ac131d72f619464bdab27b53ff73
Jervy Terte <jervz@users.noreply.github.com>
2018-02-01 07:29:24 +1300

    Test

--- picker/picker.js
+++ picker/picker.js
@@ -11,24 +11,14 @@ angular.module('myApp.picker', ['ngRoute', 'firebase'])
 
     .controller('pickerCtrl', ['$scope', '$firebaseArray', function ($scope, $firebaseArray) {
 
-        //// Initialize Firebase
-        //var config = {
-        //    apiKey: "AIzaSyDDR0N4N6hKb4X_r7-BvGVRtPgTMaFvbUg",
-        //    authDomain: "heaven-dev-db.firebaseapp.com",
-        //    databaseURL: "https://heaven-dev-db.firebaseio.com",
-        //    projectId: "heaven-dev-db",
-        //    storageBucket: "heaven-dev-db.appspot.com",
-        //    messagingSenderId: "917810951004"
-        //};
-        //firebase.initializeApp(config);
 
         $scope.addPicker = function() {
 
-            var ref = firebase.database().ref("pickers");
+            var ref = firebase.database().ref("Picker");
             $firebaseArray(ref).$add($scope.picker)
                 .then(function(ref) {
                     var id = ref.key;
-                    console.log('Added New Picker ' + id);
+                    alert('Added New Picker ' + id);
 
                     $scope.picker.FirstName = '';
                     $scope.picker.LastName = '';
@@ -36,28 +26,7 @@ angular.module('myApp.picker', ['ngRoute', 'firebase'])
 
                 });
         }
+         
 
-        //var ref = firebase.database().ref().child("pickers");
-        //$scope.pickers = $firebaseArray(ref);
-
-        //$scope.addPicker = function() {
-
-
-        //    $scope.pickers.$add({
-
-        //        FirstName: $scope.firstName,
-        //        LastName: $scope.lastName,
-        //        NicktName: $scope.nicktName
-
-        //    }).then(function(ref) {
-        //        var id = ref.key;
-        //        console.log('Addes Picker ' + id);
-
-        //        $scope.firstName = '';
-        //        $scope.lastName = '';
-        //        $scope.nicktName = '';
-
-        //    });
-    
 
 }]);
