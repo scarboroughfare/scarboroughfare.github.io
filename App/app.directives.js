641c52a65c9ec12472fde38cfec167bc429eb5ef
Jervy Terte <jervz@users.noreply.github.com>
2018-02-01 18:35:44 +1300

    Responsive Buttons for mobile

---
+++ App/app.directives.js
@@ -0,0 +1,25 @@
+﻿heavenApp.directive('myDirective', ['$window', function ($window) {
+    return {
+        link: link,
+        restrict: 'A'
+    };
+    function link(scope, element, attrs) {
+        scope.width = $window.innerWidth;
+
+        function onResize() {
+            console.log($window.innerWidth);
+            // uncomment for only fire when $window.innerWidth change   
+            if (scope.width !== $window.innerWidth) {
+                scope.width = $window.innerWidth;
+                scope.$digest();
+            }
+        };
+
+        function cleanUp() {
+            angular.element($window).off('resize', onResize);
+        }
+
+        angular.element($window).on('resize', onResize);
+        scope.$on('$destroy', cleanUp);
+    }
+}]);
