fec8aa02410b6bdbf59d08592735ae9fc6e8f38c
Jervy Terte <jervz@users.noreply.github.com>
2018-02-15 16:55:14 +1300

    Added ui-sortable

--- App/app.module.js
+++ App/app.module.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var heavenApp = angular.module('heavenApp', ['ngRoute', 'ngAnimate', 'ngSanitize', 'firebase', 'ui.bootstrap', 'xeditable', 'checklist-model', 'toastr', 'bsLoadingOverlay', 'bsLoadingOverlaySpinJs'])
+var heavenApp = angular.module('heavenApp', ['ngRoute', 'ngAnimate', 'ngSanitize', 'firebase', 'ui.bootstrap', 'xeditable', 'ui.sortable', 'checklist-model', 'toastr', 'bsLoadingOverlay', 'bsLoadingOverlaySpinJs'])
     .run(function (bsLoadingOverlayService) {
         bsLoadingOverlayService.setGlobalConfig({
             //templateUrl: 'App/shared/spinner-template.html'
